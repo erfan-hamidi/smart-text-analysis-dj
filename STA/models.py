dee9dba0d7d0bf598f2a3bed93f38cb8c18af002
erfan-hamidi <erfan9757@gmail.com>
2023-07-29 18:38:12 +0330

    add user model for signup and signin

--- STA/models.py
+++ STA/models.py
@@ -1,3 +1,35 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager,PermissionsMixin
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self,username,email,password=None):
+        if not email:
+            raise ValueError('The Email field must be set!')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, username, password=None):
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self) -> str:
+        return self.email
