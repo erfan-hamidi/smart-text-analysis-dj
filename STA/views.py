989c4d1880abdcaa04f9fca073a44ba9b1084548
Erfan-Hamidi <erfan9757@gmail.com>
2023-08-06 13:22:13 +0330

    fix views

--- STA/views.py
+++ STA/views.py
@@ -11,6 +11,10 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
+from langdetect import detect
+import h5py
+import tensorflow as tf
+import numpy as mp
 
 # views here.
 
@@ -54,8 +58,15 @@ class STAApiView(APIView):
 
     def post(self, request):
         data = request.data
-        if data['output-type'] == 'sem' :
-            pass
+        if data['output-type'] == 'sen' :
+            lang = detect(data['input'])
+            print(lang)
+            if lang == 'fa':
+                f = h5py.File('Per_Sen_Analysis_Model.hdf5','r')
+            elif lang == 'en':
+                new_model = tf.keras.models.load_model('C:\code\smart-text-analysis-dj\STA\EN_Sen_Analysis_Model.h5')
+                new_model.summary()
+                print(new_model.predict(mp.array(['The TensorFlow platform helps you implement best practices for data automation, model tracking, performance monitoring, and model retraining.Using production-level tools to automate and track model training over the lifetime of a product, service, or business process is critical to success. TFX provides software frameworks and tooling for full MLOps deployments, detecting issues as your data and models evolve over time.'])))
         elif data['output-type'] == 'sum':
             pass
         elif data['output-type'] == 'spa':
